b2a9bdd67a0373f15388ef10a645448a96530ae1
Alon Wengierko <alon.wengierko@gmail.com>
2024-04-28 17:13:01 -0300

    chore: script to get issuers of bond in USD

---
+++ scripts/get_issuers.py
@@ -0,0 +1,23 @@
+import re
+import json
+from datetime import date
+
+from bf4py import BF4Py
+
+mic = 'XFRA'
+bf4py = BF4Py(default_mic=mic) 
+search_dict = bf4py.bonds.search_parameter_template()
+search_dict['currencies'].append('USD')
+result = bf4py.bonds.search(params=search_dict)
+
+issuers = []
+for bond_object in result:
+    bond_title = bond_object['name']['originalValue']
+    issuer = re.match(r'^.*?(?=\d)', bond_title).group(0)
+    if issuer not in issuers and issuer is not "":
+        issuers.append(issuer)
+
+issuers_list = [{'issuer': issuer} for issuer in issuers]
+
+with open('issuers.json', 'w') as file:
+    file.write(json.dumps(issuers_list, indent=4))
